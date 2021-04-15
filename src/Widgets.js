990fb1aef48a3c0f936d607a629ed1e14d0aaf89
Tanay5175 <71628305+Tanay5175@users.noreply.github.com>
2021-04-16 01:03:55 +0530

    Update Widgets.js

--- src/Widgets.js
+++ src/Widgets.js
@@ -6,7 +6,7 @@ function Widgets() {
         <div className="widgets">
             <h1>MEET THE CREATOR</h1>
             <iframe
-            src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Ftanay.agarwal.359%2Fposts%2F1831981166966845%3A0&width=500&show_text=true&height=534&appId" width="340" height="100%" style={{ border: "none", overflow: "hidden" }} scrolling="no" frameBorder="0" allowTransparency="true" allow="encrypted-media"
+            src=""
             ></iframe>   
         </div>
     )
